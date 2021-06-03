0b6662fb7e629e15a15b457616bf21f37831012c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-06-03 19:53:45 +0300

    Добавьте файлы проекта.

---
+++ CPPThirdAPP.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+// ТОВАР: МНОГОФАЙЛОВЫЕБ
+// ЕНАМЫ: всякая всячина бла-бла-бла... (БЕЗ УТОЧНЕНИЯ МОДЕЛИ, МАРКИ И ТАК ДАЛЕЕ (АБСТРАКТНОЕ ОПРЕДЕЛЕНИЕ))
+// КЛАСС: ТОВАР
+enum Category
+{
+	Electronics = 1,
+	Furniture,	// - мебель
+	Dishes		// - посуда
+};
+
+class C_Product
+{
+public:
+	Product()
+	{
+
+	}
+
+	Product()
+	{
+
+	}
+
+private:
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "​Russian");
+}
