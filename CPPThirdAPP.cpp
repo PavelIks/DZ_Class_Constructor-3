ca90d4833d0bec3b965579c9a1804c2abab8d743
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-06-07 16:02:38 +0300

    Updated-1.

--- CPPThirdAPP.cpp
+++ CPPThirdAPP.cpp
@@ -3,31 +3,62 @@
 // ТОВАР: МНОГОФАЙЛОВЫЕБ
 // ЕНАМЫ: всякая всячина бла-бла-бла... (БЕЗ УТОЧНЕНИЯ МОДЕЛИ, МАРКИ И ТАК ДАЛЕЕ (АБСТРАКТНОЕ ОПРЕДЕЛЕНИЕ))
 // КЛАСС: ТОВАР
-enum Category
+enum E_Categorys
 {
-	Electronics = 1,
-	Furniture,	// - мебель
-	Dishes		// - посуда
+	Categorys_Electronic,
+	Categorys_Furniture,		// - мебель
+	Categorys_Dishes			// - посуда
 };
 
 class C_Product
 {
 public:
-	Product()
+	C_Product()
 	{
-
+		name_product = (char*)"Товар...";
+		categorys = E_Categorys::Categorys_Electronic;
+	}
+	C_Product(char* _name_product, E_Categorys _categorys)
+	{
+		name_product = _name_product;
+		categorys = _categorys;
 	}
 
-	Product()
+	void void_categorys(E_Categorys e_categorys)
 	{
+		if (e_categorys == Categorys_Electronic)
+		{
+			std::cout << (char*)"Электроника";
+		}
+		else if (e_categorys == Categorys_Furniture)
+		{
+			std::cout << (char*)"Мебель";
+		}
+		else if (e_categorys == Categorys_Dishes)
+		{
+			std::cout << (char*)"Посуда";
+		}
+		else
+		{
+			std::cout << (char*)"?";
+		}
+	}
 
+	void show_info()
+	{
+		std::cout << "Имя товара: " << name_product << "\n";
+		std::cout << "Категория товара: " << categorys << "\n\n";
 	}
 
 private:
-
+	char* name_product;
+	E_Categorys categorys;
 };
 
 int main()
 {
-	setlocale(LC_ALL, "​Russian");
+	setlocale(LC_ALL, "Russian");
+
+	C_Product product1((char*)"Стул", Categorys_Furniture);
+	product1.show_info();
 }
